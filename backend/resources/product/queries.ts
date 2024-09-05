838173decc1418fabd85515dfea6930221bdb4b8
JohanTran <johan.tran0210@gmail.com>
2024-09-05 18:05:36 +0200

    resolved #14

--- backend/resources/product/queries.ts
+++ backend/resources/product/queries.ts
@@ -1,4 +1,4 @@
-import { GraphQLList, GraphQLObjectType } from "graphql";
+import { GraphQLID, GraphQLList, GraphQLObjectType } from "graphql";
 import { Product } from "../../models/models";
 import { ProductType } from "../../models/schema";
 
@@ -9,6 +9,15 @@ export const productQuery = new GraphQLObjectType({
             type: new GraphQLList(ProductType),
             resolve: async () => {
                 return await Product.find({});
+            }
+        },
+        product: {
+            type: ProductType,
+            args: {
+                _id: { type: GraphQLID },
+            },
+            resolve: async (_, { _id }) => {
+                return await Product.findById(_id);
             }
         }
     })
