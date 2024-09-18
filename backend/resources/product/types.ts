e7c2cee7fc2b0417e89c1adf4ed9f660b678537b
JohanTran <johan.tran0210@gmail.com>
2024-09-18 13:20:07 +0200

    set up apollo client done

--- backend/resources/product/types.ts
+++ backend/resources/product/types.ts
@@ -1,4 +1,3 @@
-//TODO #25 Import the interface instead of defining it in every file @JohanTran02
 import { IManufacturer } from "../manufacturer/types";
 
 export interface IProduct {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,52 @@
+import { gql, useQuery } from "@apollo/client";
 import { useState } from "react"
+import { IProduct } from "../../backend/resources/product/types.ts"
+
+const GET_PRODUCTS = gql`
+    query RootQuery($limit : Int) {  
+        product{
+            products(limit: $limit){
+            name
+            sku
+            description
+            price
+            category
+            amountInStock
+                manufacturer{
+                    name
+                    description
+                    website
+                    address
+                    contact{
+                        name
+                        phone
+                        email
+                    }
+                }
+            }
+        }
+    }
+`;
+
+// name: string,
+// sku: string,
+// description: string,
+// price: number,
+// category: string,
+// manufacturer: IManufacturer,
+// amountInStock: number
 
 function App() {
     const [activeTab, setActiveTab] = useState<"product" | "manufacturer">("product");
+    const { error, loading, data: { product: { products } = { products: [] } } = {} } = useQuery<{ product: { products: IProduct[] } }>(GET_PRODUCTS, {
+        variables: { limit: 10 }
+    })
+
+    if (loading) return null;
+    if (error) return `Error! ${error.message}`;
+
+    console.log(products);
+    const product = products[2];
 
     return (
         <main className="flex flex-row h-screen max-h-screen w-screen">
@@ -20,13 +65,13 @@ function App() {
                         </div>
                         {
                             activeTab === "product" ? <>
-                                <input type="text" disabled value={"Product name"} className="h-12" />
-                                <input type="text" disabled value={"Product category"} className="h-12" />
+                                <input type="text" disabled value={`${product.name}`} className="h-12" />
+                                <input type="text" disabled value={`${product.category}`} className="h-12" />
                                 <div className="flex gap-4">
-                                    <input type="text" disabled value={"Product name"} className="h-12 flex-1 w-0" />
-                                    <input type="text" disabled value={"Product name"} className="h-12 flex-1 w-0" />
+                                    <input type="text" disabled value={`${product.price}`} className="h-12 flex-1 w-0" />
+                                    <input type="text" disabled value={`${product.amountInStock}`} className="h-12 flex-1 w-0" />
                                 </div>
-                                <textarea disabled value={"Product description"} className="resize-none h-[200px]" />
+                                <textarea disabled value={`${product.description}`} className="resize-none h-[200px]" />
                             </> :
                                 <>
                                     <input type="text" disabled value={"Manufacturer name"} className="h-12" />
