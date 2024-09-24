ec8f6de95013ee9c94f7966a086ccb4bd638233e
JohanTran <johan.tran0210@gmail.com>
2024-09-24 18:21:04 +0200

    removed unnecessary design

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-// import App from './App.tsx'
+import App from "./App";
 import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
@@ -11,14 +11,10 @@ const client = new ApolloClient({
 
   cache: new InMemoryCache(),
 });
-import { RouterProvider } from "react-router-dom";
-import App from "./App";
-// import router from './routes/routes.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      {/* <RouterProvider router={router} /> */}
       <Provider store={store}>
         <App />
       </Provider>
