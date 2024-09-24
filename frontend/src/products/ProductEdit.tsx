392e0985d77729c7597a2fbf6a6e7cedbc0c73f4
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-09-24 15:00:54 +0200

    added minor styling

--- frontend/src/products/ProductEdit.tsx
+++ frontend/src/products/ProductEdit.tsx
@@ -7,7 +7,7 @@ import FormPicker from "../components/FormPicker";
 export function ProductEdit() {
     return (
       <>
-        <div className="bg-purple-300 flex-1 flex flex-col gap-4 ">
+        <div className=" flex-1 flex flex-col gap-4 ">
      
             <div className="flex flex-col flex-1 gap-4">
               
--- frontend/src/products/ProductLayout.tsx
+++ frontend/src/products/ProductLayout.tsx
@@ -4,9 +4,9 @@ export function ProductLayout() {
     return (
       <>
         <div className="bg-blue-300 h-full flex flex-col gap-4">
-          <div className="bg-blue-100 flex-auto min-w-[75%] max-h-[40%]  ">
-            <h2 className="m-8"> Edit Product</h2>
-            <div className="text-gray-400 max-w-[50%]  max-h-36 m-8">
+          <div className="bg-white rounded-lg flex-auto min-w-[75%] max-h-[40%]  ">
+            <h2 className="m-8 font-semibold underline text-xl "> Edit Product</h2>
+            <div className="text-gray-500 max-w-[50%]  max-h-36 m-8">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
               laudantium odit, corrupti cumque consequuntur expedita fugiat fuga
               quidem quia harum. Fugiat ea minima numquam autem voluptatem
